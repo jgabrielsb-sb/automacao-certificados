4a02c366bc0c576d537610eef90d8200f9406737
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-03 15:22:22 -0300

    chore

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,26 +1,12 @@
-from automacao_certificados.selenium_automations.adapters.api_requester.certificado_api_requester import CertificadoAPIRequester
-from automacao_certificados.selenium_automations.core.models import (
-    dto_supplier,
-    dto_document
-)
-
-from datetime import date
-
-BASE_URL = "http://localhost:8000"
-
-api_requester = CertificadoAPIRequester(
-    base_url=BASE_URL
-)
+from automacao_certificados.selenium_automations.websites.certidao_estadual_al.use_cases import get_certificado_using_groq
+from selenium.webdriver.chrome.webdriver import WebDriver
+from pathlib import Path
+driver = WebDriver()
 
 if __name__ == "__main__":
-    document_response = api_requester.get_document(
-        filter=dto_document.DocumentFilter(
-            supplier_id=1,
-            document_type_id=1,
-            identifier="12345678912",
-            expiration_date=date(2025, 12, 31)
-        )
+    get_certificado_using_groq(
+        driver=driver,
+        state_value="AL",
+        inscricao_value="60604235",
+        img_path_to_save=Path("src/automacao_certificados/data/certificados_caixa"),
     )
-    print(document_response)
-
-   
