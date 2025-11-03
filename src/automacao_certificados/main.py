8ff3921368936a8b33e2677a26257027379679af
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-03 14:13:22 -0300

    chore

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,30 +1,26 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
+from automacao_certificados.selenium_automations.adapters.api_requester.certificado_api_requester import CertificadoAPIRequester
+from automacao_certificados.selenium_automations.core.models import (
+    dto_supplier,
+    dto_document
+)
 
-from automacao_certificados.selenium_automations.websites.certidao_estadual_al.pages import ConsultaPage
-from automacao_certificados.selenium_automations.adapters.image_processor.groq_image_processor import GroqImageProcessor
+from datetime import date
 
-from automacao_certificados.config import settings
+BASE_URL = "http://localhost:8000"
 
-from pathlib import Path
-import time
-
-from automacao_certificados.selenium_automations.websites.certidao_estadual_al.use_cases import download_certificado_by_cnpj_using_groq
-
+api_requester = CertificadoAPIRequester(
+    base_url=BASE_URL
+)
 
 if __name__ == "__main__":
-    driver = WebDriver()
-    CNPJ = "60604235"
-
-    download_certificado_by_cnpj_using_groq(
-        driver=driver,
-        state_value="AL",
-        captcha_adapter=GroqImageProcessor(
-            groq_api_key=settings.groq_api_key,
-        ),
-        inscricao_value=CNPJ,
-        img_path_to_save=Path("/home/jgabrielsb/Documents/Programming/Sebrae/automacao-certificados/src/automacao_certificados/data/certificados/certificado.png"),
+    document_response = api_requester.get_document(
+        filter=dto_document.DocumentFilter(
+            supplier_id=1,
+            document_type_id=1,
+            identifier="12345678912",
+            expiration_date=date(2025, 12, 31)
+        )
     )
-
-    time.sleep(1000)
+    print(document_response)
 
    
