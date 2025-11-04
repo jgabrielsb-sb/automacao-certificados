6e60c75ea7f92059845402d2892524a09c3f5441
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-04 12:19:20 -0300

    refac: changing exceptions

--- src/automacao_certificados/selenium_automations/adapters/api_requester/exceptions.py
+++ src/automacao_certificados/selenium_automations/adapters/api_requester/exceptions.py
@@ -5,25 +5,18 @@ class ConflictError(APIRequesterException):
     def __init__(
         self, 
         route: str,
-        object: str,
-        resource_name: str,
-        resource_value: str
+        message: str
     ) -> None:
-        message = f"Object {object} already exists with {resource_name}:{resource_value}"
         super().__init__(message)
-
         self.route = route
-        self.object = object
-        self.resource_name = resource_name
-        self.resource_value = resource_value
+        self.message = message
 
 class RouteNotFoundError(APIRequesterException):
     def __init__(
         self,
         route: str,
-        message: str
+        message: str,
     ) -> None:
-        message = f"Route {route} not found. Please check if the route is correct."
         super().__init__(message)
         self.route = route
         self.message = message
@@ -32,22 +25,19 @@ class NotFoundError(APIRequesterException):
     def __init__(
         self,
         route: str,
-        message: str
+        message: str,
     ):
-        message = f"Route {route} error: {message}"
         super().__init__(message)
         self.route = route
         self.message = message
-
+        
 class BadRequestError(APIRequesterException):
     def __init__(
         self,
         route: str,
         message: str
     ) -> None:
-        message = f"Bad request: {message}"
         super().__init__(message)
-
         self.route = route
         self.message = message
 
@@ -58,9 +48,7 @@ class UnexpectedError(APIRequesterException):
         message: str,
         status_code: str,
     ) -> None:
-        message = f"Unexpected error: {message}"
         super().__init__(message)
-
         self.route = route
-        self.message = message
         self.status_code = status_code
+        self.message = message
