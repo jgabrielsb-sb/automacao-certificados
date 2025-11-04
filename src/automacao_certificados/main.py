825d0302fb471d00d03a5fae01e96d0022f1595b
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-04 14:41:56 -0300

    chore

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,22 +1,11 @@
-from automacao_certificados.selenium_automations.websites.certidao_estadual_al.use_cases import get_certificado_using_groq
-from automacao_certificados.selenium_automations.websites.certidao_municipal_maceio.use_cases import download_certificado
-from selenium.webdriver.chrome.webdriver import WebDriver
-from pathlib import Path
-
-from automacao_certificados.selenium_automations.adapters.extractors import CertificadoMaceioExtractor
-
-driver = WebDriver()
-default_path = Path("/home/jgabrielsb/Downloads")
-
-# extractor = CertificadoMaceioExtractor(
-#     pdf_path=Path("certidao_maceio.pdf"),
-# )
+from automacao_certificados.selenium_automations.adapters.http import HttpxClient
 
 if __name__ == "__main__":
-    download_certificado(
-        driver=driver,
-        cnpj="60604235000114",
-        img_path_to_save=default_path,
+    client = HttpxClient()
+    
+    response = client.get(
+        "http://localhost:8000/api/v1/documents//",
     )
 
-    #print(extractor.run())
+    print(response)
+    
