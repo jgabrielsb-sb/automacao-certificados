41b3c6e6793d7d16722a05f8422592dbc3f49744
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-06 12:37:47 -0300

    feat: validate_document_file and tests

--- src/automacao_certificados/selenium_automations/utils/utils.py
+++ src/automacao_certificados/selenium_automations/utils/utils.py
@@ -1,4 +1,4 @@
-
+import base64
 
 def format_cnpj(cnpj: str) -> str:
     """
@@ -12,9 +12,24 @@ def format_cnpj(cnpj: str) -> str:
     """
     if not isinstance(cnpj, str):
         raise ValueError("cnpj must be a string")
-    if len(cnpj) != 14:
-        raise ValueError("cnpj must be 14 digits")
     if not cnpj.isdigit():
         raise ValueError("cnpj must be a number")
+    if len(cnpj) != 14:
+        raise ValueError("cnpj must have 14 digits")
 
     return f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}"
+
+def validate_document_file(base64_pdf: str) -> None:
+    if not isinstance(base64_pdf, str):
+        raise ValueError("base64_pdf must be a str")
+
+    try:
+        pdf_bytes = base64.b64decode(base64_pdf, validate=True)
+    except Exception as e:
+        raise ValueError("invalid base64 string: ", e)
+
+    if not pdf_bytes.startswith(b"%PDF-"):
+        raise ValueError("base64_pdf is not a valid pdf file: it does not contain %PDF on the beggining")
+    
+    if b"%%EOF" not in pdf_bytes:
+        raise ValueError("base64_pdf is not a valid pdf file: it does not contains %%EOF")
