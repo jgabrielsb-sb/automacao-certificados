ae0548396faddab0de0fda2d1e3063e5f17d0f77
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-12 14:07:39 -0300

    refac: changing certidal estadual selenium workflow structure

--- src/automacao_certificados/selenium_automations/adapters/selenium_workflow/certidao_estadual_selenium_workflow.py
+++ src/automacao_certificados/selenium_automations/adapters/selenium_workflow/certidao_estadual_selenium_workflow.py
@@ -2,7 +2,6 @@ from automacao_certificados.selenium_automations.core.interfaces import (
     SeleniumWorkflowPort,
     SeleniumWorkflowInput,
     SeleniumWorkflowOutput,
-
     ImageProcessorPort,
     SeleniumCaptchaGatewayPort,
 )
@@ -13,7 +12,10 @@ from automacao_certificados.selenium_automations.websites.certidao_estadual_al i
     DownloadPage
 )
 
-from automacao_certificados.selenium_automations.websites.certidao_estadual_al.exceptions import InvalidCaptchaException
+from automacao_certificados.selenium_automations.application.services import (
+    SolveCaptchaService
+)
+
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from pathlib import Path
@@ -23,54 +25,27 @@ class CertidaoEstadualALSeleniumWorkflow(SeleniumWorkflowPort):
         self,
         driver: WebDriver,
         img_path_to_save_file: Path,
-        image_processor: ImageProcessorPort,
-        captcha_gateway: SeleniumCaptchaGatewayPort,
     ):
-        if not isinstance(image_processor, ImageProcessorPort):
-            raise ValueError("image_processor must be a ImageProcessorPort")
-        
-        if not isinstance(captcha_gateway, SeleniumCaptchaGatewayPort):
-            raise ValueError("captcha_gateway must be a CaptchaGatewayPort")
-        
         if not isinstance(img_path_to_save_file, Path):
             raise ValueError("img_path_to_save_file must be a Path")
         
         self.driver = driver
-        self.image_processor = image_processor
         self.img_path_to_save_file = img_path_to_save_file
-        self.captcha_gateway = captcha_gateway
-
-    def _solve_captcha(self, cnpj=str) -> int:
         
-        captcha_passed = False
-        attempts = 0
-        
-        while not captcha_passed:
-            try:
-                ConsultaPage(
-                    self.driver,
-                    self.image_processor,
-                    self.captcha_gateway
-                ).run(
-                    state_value='AL',
-                    tipo_inscricao_value='CNPJ',
-                    inscricao_value=cnpj
-                )
-                captcha_passed = True
-            except InvalidCaptchaException:
-                attempts += 1
-                pass
-
-        return attempts
-
-
     def get_document(
         self, 
         input: SeleniumWorkflowInput
     ) -> SeleniumWorkflowOutput:
+        
+        """
+        deal with the process of consulting the cnpj cert.
+        here, i would expect 
+        """
+        ConsultaPage(
+            ...
+        ).run()
 
-        self._solve_captcha(input.supplier_cnpj)
-
+        # pass the download page
         document_extracted, base64_pdf = DownloadPage(
             self.driver
         ).run(self.img_path_to_save_file)
