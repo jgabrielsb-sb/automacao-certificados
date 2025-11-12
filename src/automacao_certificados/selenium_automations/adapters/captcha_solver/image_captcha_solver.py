1f53406aedf09af2d520566d084c88050a35174d
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-12 15:19:42 -0300

    feat/tests: image captcha solver adapter and tests

---
+++ src/automacao_certificados/selenium_automations/adapters/captcha_solver/image_captcha_solver.py
@@ -0,0 +1,28 @@
+from automacao_certificados.selenium_automations.core.interfaces import *
+
+
+class ImageCaptchaSolver(CaptchaSolverPort):
+    """
+    Solves the captcha by getting the captcha text from the image 
+    and inserting it on the input field.
+    """
+    def __init__(
+        self,
+        image_processor: ImageProcessorPort,
+        captcha_gateway: SeleniumCaptchaGatewayPort
+    ):
+        if not isinstance(image_processor, ImageProcessorPort):
+            raise ValueError("image_processor must be a ImageProcessorPort")
+        
+        if not isinstance(captcha_gateway, SeleniumCaptchaGatewayPort):
+            raise ValueError("captcha_gateway must be a SeleniumCaptchaGatewayPort")
+        
+        self.image_processor = image_processor
+        self.captcha_gateway = captcha_gateway
+
+    def solve_captcha(self, input: CaptchaSolverInput) -> str:
+        base64_img = self.captcha_gateway.get_captcha_base64_img(input.img_webelement)
+        text = self.image_processor.get_text(base64_img)
+        self.captcha_gateway.fill_captcha_text(input.input_webelement, text)
+        return text
+
---
+++ tests/selenium_automations/adapters/captcha_solver/test_image_captcha_solver.py
@@ -0,0 +1,59 @@
+from mailbox import Mailbox
+from automacao_certificados.selenium_automations.adapters.captcha_solver import ImageCaptchaSolver, image_captcha_solver
+from automacao_certificados.selenium_automations.core.interfaces import *
+
+import pytest
+
+from unittest.mock import MagicMock, Mock
+from selenium.webdriver.remote.webelement import WebElement
+
+
+
+
+
+class TestInit:
+    def test_if_raises_value_error_if_image_processor_is_not_a_image_processor_port(self):
+        """
+        Test if the ImageCaptchaSolver raises a ValueError if the image_processor is not a ImageProcessorPort.
+        """
+        with pytest.raises(ValueError) as e:
+            ImageCaptchaSolver(
+                image_processor="not_a_image_processor_port",
+                captcha_gateway=Mock(spec=SeleniumCaptchaGatewayPort),
+            )
+
+        assert "image_processor" in str(e.value)
+
+    def test_if_raises_value_error_if_captcha_gateway_is_not_a_captcha_gateway_port(self):
+        """
+        Test if the ImageCaptchaSolver raises a ValueError if the captcha_gateway is not a SeleniumCaptchaGatewayPort.
+        """
+        with pytest.raises(ValueError) as e:
+            ImageCaptchaSolver(
+                image_processor=Mock(spec=ImageProcessorPort),
+                captcha_gateway="not_a_captcha_gateway_port",
+            )
+
+        assert "captcha_gateway" in str(e.value)
+
+class TestSolveCaptcha:
+    def test_if_returns_the_captcha_text(
+        self,
+        monkeypatch
+    ):
+        image_processor = MagicMock(spec=ImageProcessorPort)
+        image_processor.get_text.return_value = "test text"
+
+        captcha_gateway = MagicMock(spec=SeleniumCaptchaGatewayPort)
+        captcha_gateway.get_captcha_base64_img.return_value = "test img"
+        captcha_gateway.fill_captcha_text.return_value = None
+        
+        text = ImageCaptchaSolver(
+            image_processor,
+            captcha_gateway
+        ).solve_captcha(input=CaptchaSolverInput(
+            input_webelement=MagicMock(spec=WebElement),
+            img_webelement=MagicMock(spec=WebElement),
+        ))
+
+        assert text == "test text"
