a18276621ffa2ee117a3236afac6e6c7b82fe1e4
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-13 15:33:51 -0300

    chore

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,11 +1,13 @@
 from automacao_certificados.selenium_automations.adapters.selenium.certidao_municipal_maceio.pages import *
 from pathlib import Path
 from selenium.webdriver.chrome.webdriver import WebDriver
+from automacao_certificados.selenium_automations.application.use_cases import download_certificado_maceio
 
 if __name__ == "__main__":
     driver = WebDriver()
-    consulta_page = ConsultaPage(
-        driver=driver,
-        img_path_to_save=Path("certificado.png"),
+    document_extracted, base64_pdf = download_certificado_maceio(
+        cnpj="32652832000189",
     )
-    consulta_page.run(cnpj="60604235")
+    print(f'document_extracted: {document_extracted} \n\n')
+    print(f'base64_pdf: {base64_pdf} \n\n')
+    driver.quit()
