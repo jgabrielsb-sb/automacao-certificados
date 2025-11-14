d56eabe67e33e355269ce06593d2433b6069f07d
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-14 11:20:53 -0300

    chore

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,13 +1,8 @@
-from automacao_certificados.selenium_automations.adapters.selenium.certidao_municipal_maceio.pages import *
-from pathlib import Path
-from selenium.webdriver.chrome.webdriver import WebDriver
-from automacao_certificados.selenium_automations.application.use_cases import download_certificado_maceio
-
+from automacao_certificados.selenium_automations.application.use_cases import download_certificado_arapiraca
+from automacao_certificados.selenium_automations.adapters.extractors import CertidaoArapiracaExtractor
 if __name__ == "__main__":
-    driver = WebDriver()
-    document_extracted, base64_pdf = download_certificado_maceio(
-        cnpj="32652832000189",
+    b64 = download_certificado_arapiraca(
+        cnpj="12345678912345"
     )
-    print(f'document_extracted: {document_extracted} \n\n')
-    print(f'base64_pdf: {base64_pdf} \n\n')
-    driver.quit()
+    
+    
