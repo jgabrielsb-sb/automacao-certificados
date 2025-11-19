77578bf79747b235a2a2b9a34222f81db89c9bad
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-19 15:45:41 -0300

    feat/tests: image processor interface and tests

--- src/automacao_certificados/selenium_automations/core/interfaces/image_processor.py
+++ src/automacao_certificados/selenium_automations/core/interfaces/image_processor.py
@@ -1,13 +1,31 @@
-from typing import Protocol, runtime_checkable
+# from typing import Protocol, runtime_checkable
 
-from pydantic import BaseModel
+# from pydantic import BaseModel
 
-@runtime_checkable
-class ImageProcessorPort(Protocol):
+# @runtime_checkable
+# class ImageProcessorPort(Protocol):
+#     """
+#     Image processors protocol.
+#     An image processor is a class that can process an image and return the text 
+#     from the image.
+#     """
+#     def get_text(self, base64_img: str) -> str: ...
+
+from abc import ABC, abstractmethod
+
+from automacao_certificados.selenium_automations.core.models import *
+from automacao_certificados.selenium_automations.core.exceptions import *
+
+class ImageProcessorPort(ABC):
     """
-    Image processors protocol.
-    An image processor is a class that can process an image and return the text 
-    from the image.
+    Image processor interface.
     """
-    def get_text(self, base64_img: str) -> str: ...
+    @abstractmethod
+    def _get_text(self, input: ImageProcessorInput) -> ImageProcessorOutput:
+        pass
     
+    def run(self, input: ImageProcessorInput) -> ImageProcessorOutput:
+        try:
+            return self._get_text(input)
+        except Exception as e:
+            raise ImageProcessorException(e)
