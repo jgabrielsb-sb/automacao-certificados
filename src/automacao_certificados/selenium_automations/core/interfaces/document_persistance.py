cfecb27a8e19278c5b313fb535fc2c36fed2f457
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-19 12:24:33 -0300

    refac/tests: using abstract method instead of protocol to wrapper run method into the propper exception and tests

--- src/automacao_certificados/selenium_automations/core/interfaces/document_persistance.py
+++ src/automacao_certificados/selenium_automations/core/interfaces/document_persistance.py
@@ -1,16 +1,37 @@
-from typing import Protocol, runtime_checkable
+# from typing import Protocol, runtime_checkable
+
+# from pydantic import BaseModel
+
+# from automacao_certificados.selenium_automations.core.models import dto_document
+
+# class DocumentPersist(BaseModel):
+#     document_extracted: dto_document.DocumentExtracted
+#     base64_pdf: str
+
+# class DocumentPersistResult(BaseModel):
+#     result: dict | str # should be passed the response of the api, for example.
+
+# @runtime_checkable
+# class DocumentPersistancePort(Protocol):
+#     def save(self, document: DocumentPersist) -> DocumentPersistResult: ...
+
+from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
 
-from automacao_certificados.selenium_automations.core.models import dto_document
+from automacao_certificados.selenium_automations.core.models import *
+from automacao_certificados.selenium_automations.core.exceptions import *
 
-class DocumentPersist(BaseModel):
-    document_extracted: dto_document.DocumentExtracted
-    base64_pdf: str
+class DocumentPersistancePort(ABC):
+    """
+    Interface responsible for save the document.
+    """
+    @abstractmethod
+    def _save(self, input: DocumentPersistanceInput):
+        pass
 
-class DocumentPersistResult(BaseModel):
-    result: dict | str # should be passed the response of the api, for example.
-
-@runtime_checkable
-class DocumentPersistancePort(Protocol):
-    def save(self, document: DocumentPersist) -> DocumentPersistResult: ...
+    def run(self, input: DocumentPersistanceInput):
+        try:
+            self._save(input)
+        except Exception as e:
+            raise DocumentPersistanceException(e)
