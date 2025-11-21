f7f7f8255bed24d84878c9668397ddbf09dd6d7d
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-21 16:18:09 -0300

    chore: removing dto municipios

--- src/automacao_certificados/selenium_automations/core/models/__init__.py
+++ src/automacao_certificados/selenium_automations/core/models/__init__.py
@@ -4,4 +4,4 @@ from .dto_document import *
 from .dto_supplier import *
 from .dto_document_type import *
 from .adapters import *
-from .dto_municipio import *
+from .application import *
--- src/automacao_certificados/selenium_automations/core/models/dto_municipio.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class MunicipioEnum(Enum):
-    ARAPIRACA = 'ARAPIRACA'
-    MACEIO = 'MACEIO'
-    DELMIRO_GOUVEIA = 'DELMIRO GOUVEIA'
-    PENEDO = 'PENEDO'
