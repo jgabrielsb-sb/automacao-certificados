7858a0f55e545dedb24cbbc7ae2c863ab0c77b15
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-21 11:37:53 -0300

    feat/tests: municipio getter port and tests

---
+++ src/automacao_certificados/selenium_automations/core/interfaces/municipio_getter.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+from automacao_certificados.selenium_automations.core.models import MunicipioEnum
+from automacao_certificados.selenium_automations.utils import validate_cnpj
+from automacao_certificados.selenium_automations.core.exceptions import *
+
+class MunicipioGetterPort(ABC):
+    """
+    Interface responsible for getting the municipality by cnpj.
+    """
+    @abstractmethod
+    def _get_municipio_by_cnpj(self, cnpj: str) -> MunicipioEnum:
+        """
+        Gets the municipality by cnpj.
+        Args:
+            cnpj: the cnpj of the supplier.
+        Returns:
+            The municipality enum.
+        """
+        pass
+
+    def run(self, cnpj: str) -> MunicipioEnum:
+        """
+        Runs the municipality getter.
+        Args:
+            cnpj: the cnpj of the supplier.
+        Returns:
+            The municipality enum.
+        """
+        validate_cnpj(cnpj)
+        try:
+            municipio = self._get_municipio_by_cnpj(cnpj)
+            if not isinstance(municipio, MunicipioEnum):
+                raise ValueError("the municipality is not a valid enum")
+            
+            return municipio
+        except Exception as e:
+            raise MunicipioGetterException(e)
