8de89762c85e2abd2b6d1040909290fd9dc9d4e0
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-24 14:46:37 -0300

    feat: implementing cron on main

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,9 +1,38 @@
-from automacao_certificados.selenium_automations.application.use_cases import download_certificado_arapiraca
-from automacao_certificados.selenium_automations.adapters.extractors import CertidaoArapiracaExtractor
-if __name__ == "__main__":
-    b64 = download_certificado_arapiraca(
-        cnpj="12345678912345"
+from automacao_certificados.selenium_automations.application import *
+from automacao_certificados.selenium_automations.adapters import *
+from automacao_certificados.selenium_automations.core.interfaces import *
+from automacao_certificados.config import settings
+
+from datetime import datetime
+import schedule
+import time
+
+
+use_case = DownloadCertificatesUseCase(
+    ppe_api_requester=PPEAPIRequester(
+        http=HttpxClient(),
+        api_key=settings.ppe_api_key),
+    workflow_selector=WorkflowSelector(
+        municipio_api_requester=ReceitaAPIMunicipioGetter(
+            api_requester=ReceitaAPIRequester(
+                http=HttpxClient(),
+            )
+        )
     )
-    print('hello')
-    
-    
+)
+
+generator = DownloadCertificatesReportGenerator(
+    save_path=Path("data/certificates_report"),
+)
+
+def run():
+    certificates = use_case.run()
+    file_path = generator.run(certificates, date=datetime.now())
+    print(f"Report generated at {file_path}")
+
+schedule.every().day.at(settings.run_cron_time).do(run)
+
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
