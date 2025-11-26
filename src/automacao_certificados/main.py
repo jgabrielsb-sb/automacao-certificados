cfd2021f5737ccc9e504e2498b3733f1a0447848
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-11-26 18:28:23 -0300

    refac: changing composition location

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,7 +1,20 @@
-from automacao_certificados.composition.container import Container
+from automacao_certificados.selenium_automations.composition.container import Container
+from automacao_certificados.selenium_automations.core.models import *
+from automacao_certificados.config import settings
+
+import schedule
+import time
 
 container = Container()
+download_certificates_use_case = container.use_case_factory.create_download_certificates_use_case()
 
-use_case = container.get_download_certificates_use_case()
+def run():
+    download_certificates_use_case.run()
 
-use_case.run()
+schedule.every().day.at(settings.run_cron_time).do(run)
+
+if __name__ == "__main__":
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
