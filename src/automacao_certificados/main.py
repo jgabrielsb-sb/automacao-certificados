007616f572b580d0456e6ec181c7dc700502edce
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-12-01 10:55:05 -0300

    chore: notyfing user when system brokes

--- src/automacao_certificados/main.py
+++ src/automacao_certificados/main.py
@@ -1,5 +1,11 @@
+from automacao_certificados.selenium_automations.application.use_cases.email_use_cases import SendApplicationBrokeReportViaEmailUseCase
 from automacao_certificados.selenium_automations.composition.container import Container
-from automacao_certificados.selenium_automations.core.models import SendDownloadCertificatesReportViaEmailUseCaseInput
+from automacao_certificados.selenium_automations.core.models import (
+    SendDownloadCertificatesReportViaEmailUseCaseInput,
+    SendApplicationBrokeReportViaEmailUseCaseInput
+)
+
+from automacao_certificados.selenium_automations.core.exceptions.application.download_certificates_exceptions import DownloadCertificatesUseCaseException
 from automacao_certificados.config import settings
 from datetime import date
 import time
@@ -7,15 +13,19 @@ import schedule
 
 container = Container()
 download_certificates_use_case = container.use_case_factory.create_download_certificates_use_case()
-send_report_use_case = container.use_case_factory.create_send_download_certificates_report_via_email_use_case()
+send_application_broke_email_use_case = container.use_case_factory.create_send_application_broke_report_via_email_use_case()
+send_report_email_use_case = container.use_case_factory.create_send_download_certificates_report_via_email_use_case()
+
+send_to_emails=["jgabrielsb2002@gmail.com"]
+sender_email=settings.email_host_user
 
 def run():
     download_certificates_output = download_certificates_use_case.run()
-    send_report_use_case.run(
+    send_report_email_use_case.run(
         input=SendDownloadCertificatesReportViaEmailUseCaseInput(
             download_certificates_output=download_certificates_output,
-            send_to_emails=["jgabrielsb2002@gmail.com"],
-            sender_email=settings.email_host_user,
+            send_to_emails=send_to_emails,
+            sender_email=sender_email,
             date=date.today()
         )
     )
@@ -23,7 +33,7 @@ def run():
 
 schedule.every().day.at(settings.run_cron_time).do(run)
 
-RUN_WITH_CRON = True
+RUN_WITH_CRON = settings.run_with_cron
 
 if __name__ == "__main__":
     if RUN_WITH_CRON:
@@ -32,5 +42,20 @@ if __name__ == "__main__":
             time.sleep(1)
     
     else:
-        run()
+        try:
+            run()
+        except DownloadCertificatesUseCaseException as e:
+            send_application_broke_email_use_case.run(
+                SendApplicationBrokeReportViaEmailUseCaseInput(
+                    send_to_emails=send_to_emails,
+                    sender_email=sender_email,
+                    error_details=f"Razão do erro: {str(e)}"
+                )
+            )
+            raise
+
+            
+
+
+        
 
