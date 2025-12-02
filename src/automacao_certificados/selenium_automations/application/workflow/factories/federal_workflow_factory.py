da71f4f3d14dbff2d7f5d3545abb7baf19ec98cb
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-12-02 16:03:44 -0300

    feat: federal workflow factory

---
+++ src/automacao_certificados/selenium_automations/application/workflow/factories/federal_workflow_factory.py
@@ -0,0 +1,45 @@
+from .base_workflow_factory import WorkflowFactory
+
+from automacao_certificados.selenium_automations.application.workflow.workflow import Workflow
+from automacao_certificados.selenium_automations.infra.webdriver import get_global_webdriver
+from automacao_certificados.selenium_automations.adapters import *
+from automacao_certificados.selenium_automations.infra.api_requester import (
+    AlagoasAPIRequester,
+    CertificadoAPIRequester,
+    PPEAPIRequester,
+    DirectDataAPIRequester
+)
+
+from automacao_certificados.config import settings
+
+class FederalWorkflowFactory(WorkflowFactory):
+    def get_workflow(self) -> Workflow:
+        #driver = get_global_webdriver()
+        http_client = HttpxClient()
+
+        document_downloader = DocumentFederalDownloader(
+            api_requester=DirectDataAPIRequester(
+                http=http_client,
+                token=settings.direct_data_api_key
+            )
+        )
+        certificado_api_persistance = CertificadoApiPersistance(
+            api_requester=CertificadoAPIRequester(
+                base_url=settings.base_certificado_api_url,
+                http=http_client
+            )
+        )
+
+        ppe_api_persistance = PPEPersistance(
+            api_requester=PPEAPIRequester(
+                http=http_client,
+                api_key=settings.ppe_api_key
+            )
+        )
+
+        return Workflow(
+            document_downloader=document_downloader,
+            certificado_api_persistance=certificado_api_persistance,
+            ppe_api_persistance=ppe_api_persistance,
+        )
+
