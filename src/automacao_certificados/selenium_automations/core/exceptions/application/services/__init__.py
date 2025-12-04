35a45817783ca356e856b1ab4068c1a859c359e8
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-12-04 16:39:25 -0300

    chore: application exceptions

---
+++ src/automacao_certificados/selenium_automations/core/exceptions/application/services/__init__.py
@@ -0,0 +1,1 @@
+from .logging_register_service_exceptions import *
---
+++ src/automacao_certificados/selenium_automations/core/exceptions/application/services/logging_register_service_exceptions.py
@@ -0,0 +1,5 @@
+
+class LoggingRegisterServiceException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
